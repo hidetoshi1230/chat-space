42899747ad4005c7766208a19e2170ddd9e061e0
hidetoshi1230 <hidetoshi1230@gmail.com>
2018-01-16 14:49:46 +0900

    本番環境テスト（.jsファイル追加）

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,70 @@
+$(function(){
+  function buildHTML(message){
+    var addimage = "";
+    if (message.image.url) {
+      addimage = `<img class = "image_size", src="${ message.image.url }">`;
+    }
+    var html = `<div class="message" data-message-id="${ message.id }">
+                  <div class="message--name">
+                    ${ message.name }
+                  </div>
+                  <div class="message--date">
+                    ${ message.date }
+                  </div>
+                  <div class="message--text">
+                    ${ message.body }
+                  </div>
+                  <div class="message--image">
+                    ${ addimage }
+                  </div>
+                </div>`
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chatspace__messages').append(html);
+      $('.chatspace__messages').animate({scrollTop: $('.chatspace__messages')[0].scrollHeight}, 'fast');
+      $('.chatspace__input--message').val('');
+    })
+    .fail(function(){
+      alert('error');
+    })
+    return false;
+  })
+
+  var interval = setInterval(function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      var id = $('.message').last().data('message-id')
+      $.ajax({
+        url: location.href,
+        data: { id: id },
+        dataType: 'json',
+      })
+      .done(function(data) {
+        data.forEach(function(message){
+          var html = buildHTML(message);
+          $('.chatspace__messages').append(html);
+        });
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      })
+    }
+    else {
+      clearInterval(interval);
+    }
+  }, 5000);
+});
